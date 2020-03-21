b30953cdc9561f9d68e534f668b2226979ca52fd
Antonio Escalera <aj@angelofdeauth.host>
2020-03-21 04:54:23 -0500

    changed header style to be compliant with go build constraints/tags

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
-/*
- * @File:     main.go
- * @Created:  2020-03-18 21:47:51
- * @Modified: 2020-03-19 20:05:05
- * @Author:   Antonio Escalera
- * @Commiter: Antonio Escalera
- * @Mail:     aj@angelofdeauth.host
- * @Copy:     Copyright © 2020 Antonio Escalera <aj@angelofdeauth.host>
- */
+// @File:     main.go
+// @Created:  2020-03-21 04:21:08
+// @Modified: 2020-03-21 04:44:58
+// @Author:   Antonio Escalera
+// @Commiter: Antonio Escalera
+// @Mail:     aj@angelofdeauth.host
+// @Copy:     Copyright © 2020 Antonio Escalera <aj@angelofdeauth.host>
 
 // Package main is the entry point of the application.
 package main
--- main_test.go
+++ main_test.go
@@ -1,13 +1,12 @@
-/*
- * @File:     main_test.go
- * @Created:  2020-03-18 21:48:02
- * @Modified: 2020-03-18 21:48:06
- * @Author:   Antonio Escalera
- * @Commiter: Antonio Escalera
- * @Mail:     aj@angelofdeauth.host
- * @Copy:     Copyright © 2020 Antonio Escalera <aj@angelofdeauth.host>
- */
+// @File:     main_test.go
+// @Created:  2020-03-18 21:48:07
+// @Modified: 2020-03-21 04:45:44
+// @Author:   Antonio Escalera
+// @Commiter: Antonio Escalera
+// @Mail:     aj@angelofdeauth.host
+// @Copy:     Copyright © 2020 Antonio Escalera <aj@angelofdeauth.host>
 
+// Package main is the entrypoint of the test suite.
 package main
 
 import (
