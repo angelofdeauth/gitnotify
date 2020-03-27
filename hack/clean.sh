cbb55f0135954fa702a7ebb31ac9f9d9bc7188f9
Antonio Escalera <aj@angelofdeauth.host>
2020-03-26 19:53:15 -0500

    fixed hack/clean.sh to not include commit sha twice

--- hack/clean.sh
+++ hack/clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @File:     clean.sh
 # @Created:  2020-03-26 02:35:47
-# @Modified: 2020-03-26 19:28:01
+# @Modified: 2020-03-26 19:52:09
 # @OA:       Antonio Escalera
 # @CA:       Antonio Escalera
 # @Mail:     aj@angelofdeauth.host
@@ -13,4 +13,4 @@ cd "$(dirname "$0")/../"
 
 if [ -e "${BUILD_PATH}" ]; then rm -rf "${BUILD_PATH}"; fi
 if [ -f "${LOG_PATH}/make.log" ] && [ "${RELEASE}" = "true" ]; then :>"${LOG_PATH}"/make.log; fi
-echo "[OK] Build ${APP}-${VERSION}-${COMMIT}-${FILE_ARCH} cleaned!"
+echo "[OK] Build ${APP}-${VERSION}-${FILE_ARCH} cleaned!"
