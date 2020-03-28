67d011dded9404fd96bd57441b1a5836f0f75059
Antonio Escalera <aj@angelofdeauth.host>
2020-03-27 20:08:05 -0500

    Changed pr script API curl command to output HTTP status code

--- hack/pr.sh
+++ hack/pr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @File:     pr.sh
 # @Created:  2020-03-27 01:04:32
-# @Modified: 2020-03-27 19:40:10
+# @Modified: 2020-03-27 20:07:20
 # @OA:       Antonio Escalera
 # @CA:       Antonio Escalera
 # @Mail:     aj@angelofdeauth.host
@@ -16,8 +16,9 @@ if [ -z "${BRANCH}" ]; then
 fi
 
 if curl -XPOST -d "{\"title\":\"${BRANCH} automatic merge to master\", \"base\":\"master\", \"head\":\"${BRANCH}\", \"body\":\"${BRANCH} automatic merge to master\"}" \
-  https://api.github.com/repos/"$(git remote -v | grep origin | head -n1 | awk '{print $2}' | cut -d':' -f2 | rev | cut -c 5- | rev)"/pulls \
-  -H "Authorization: token ${GITHUB_TOKEN}" > /dev/null; then
+  -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/\
+  "$(git remote -v | grep origin | head -n1 | awk '{print $2}' | cut -d':' -f2 | rev | cut -c 5- | rev)"/pulls \
+  -H "Authorization: token ${GITHUB_TOKEN}" ; then
   echo "[OK] PR created for branch ${BRANCH} successfully!"
 else
   echo "[ERROR] PR creation for branch ${BRANCH} failed!"
