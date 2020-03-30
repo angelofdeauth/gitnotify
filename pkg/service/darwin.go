82c2b78b878fc477555f29272355dddfef2bebc6
Antonio Escalera <aj@angelofdeauth.host>
2020-03-30 00:46:19 -0500

    feat(app/command/service): Darwin service cmd conditional automatic start
    Add support for service command has optional flag to support automatically start daemon after creating resources for automatic startup.
    
    Implements support for the aforementioned under `Darwin` based operating systems.
    
    [issue #9]
    
    * Tags: os/Darwin feature app/cmd service start

--- pkg/service/darwin.go
+++ pkg/service/darwin.go
@@ -1,6 +1,6 @@
 // @File:     darwin.go
 // @Created:  2020-03-23 18:30:06
-// @Modified: 2020-03-29 12:46:15
+// @Modified: 2020-03-29 23:58:37
 // @Author:   Antonio Escalera
 // @Commiter: Antonio Escalera
 // @Mail:     aj@angelofdeauth.host
@@ -8,10 +8,27 @@
 
 package service
 
-import "github.com/angelofdeauth/xnotify/pkg/rtc"
+import (
+	"log"
+	"os/exec"
 
-// applyStartupRscDarwin applies the
-func applyStartupRscDarwin(rtc *rtc.RunTimeCfg) error {
+	"github.com/angelofdeauth/xnotify/pkg/rtc"
+)
+
+// launchctlLoad is the function to load the plist into launchctl.
+func launchctlLoad(srv string) ([]byte, error) {
+	return exec.Command("/bin/launchctl", "load", "-w", srv).CombinedOutput()
+}
+
+// startDaemonDarwin applies the
+func startDaemonDarwin(rtc *rtc.RunTimeCfg) error {
+
+	o, err := launchctlLoad(rtc.OutputPath)
+	if err != nil {
+		return err
+	}
+	log.Printf("[launchctl output]: %s", o)
+
 	return nil
 }
 
@@ -36,8 +53,8 @@ func Darwin(rtc *rtc.RunTimeCfg) error {
 	if err := createStartupRscDarwin(rtc); err != nil {
 		return err
 	}
-	if rtc.Apply {
-		if err := applyStartupRscDarwin(rtc); err != nil {
+	if rtc.Start {
+		if err := startDaemonDarwin(rtc); err != nil {
 			return err
 		}
 	}
