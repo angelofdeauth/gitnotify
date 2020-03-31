6c5eba20319c58b7b125e325cb8a1c1e5c00ff9e
Antonio Escalera <aj@angelofdeauth.host>
2020-03-31 00:41:38 -0500

    feat(app/command/service): Linux service cmd conditional automatic start
    Add support for service command has optional flag to support automatically start daemon after creating resources for automatic startup.
    
    Implements support for the aforementioned under `Linux` based operating systems.
    
    [issue #9]
    
    * Tags: os/Linux feature app/cmd service start

--- pkg/service/linux.go
+++ pkg/service/linux.go
@@ -1,6 +1,6 @@
 // @File:     linux.go
 // @Created:  2020-03-23 19:27:52
-// @Modified: 2020-03-29 23:58:50
+// @Modified: 2020-03-30 23:40:26
 // @Author:   Antonio Escalera
 // @Commiter: Antonio Escalera
 // @Mail:     aj@angelofdeauth.host
@@ -8,10 +8,44 @@
 
 package service
 
-import "github.com/angelofdeauth/xnotify/pkg/rtc"
+import (
+	"log"
+	"os/exec"
+
+	"github.com/angelofdeauth/xnotify/pkg/rtc"
+)
+
+// systemctlStartCmd is the function to load and start the service.
+func systemctlStartCmd(srv string) error {
+
+	strdr, err := exec.Command("systemctl", "daemon-reload").CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Printf("%s", strdr)
+
+	stren, err := exec.Command("systemctl", "enable", srv).CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Printf("%s", stren)
+
+	strst, err := exec.Command("systemctl", "start", srv).CombinedOutput()
+	if err != nil {
+		return err
+	}
+	log.Printf("%s", strst)
+
+	return nil
+}
 
 // startDaemonLinux applies the
 func startDaemonLinux(rtc *rtc.RunTimeCfg) error {
+
+	if err := systemctlStartCmd(rtc.App); err != nil {
+		return err
+	}
+
 	return nil
 }
 
