4272776c782224238e070973ff07e0e91b1a1953
Antonio Escalera <aj@angelofdeauth.host>
2020-04-01 15:04:59 -0500

    feat(build/make/commit): Commit script tests for gitcomm, uses container if not found
    Add support for custom commit/tag message formatter,
    github.com/angelofdeauth/gitcomm.
    Add support for container if gitcomm tool not found to maintain
    implementation goals of the build system.
    
    * Tags: build/make commit gitcomm

--- hack/commit.sh
+++ hack/commit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # @File:     commit.sh
 # @Created:  2020-03-26 20:27:09
-# @Modified: 2020-03-27 19:52:50
+# @Modified: 2020-04-01 14:01:38
 # @OA:       Antonio Escalera
 # @CA:       Antonio Escalera
 # @Mail:     aj@angelofdeauth.host
@@ -12,31 +12,63 @@ set  -ex
 cd "$(dirname "$0")/../"
 
 conditionalAddAll() {
-  git status
+  git diff
   read -r -p "Add all files (y/n)?" choice
   case "$choice" in 
-    y|Y ) echo "[OK] Adding all files..." && git add . && git commit && git push;;
+    y|Y ) echo "[OK] Adding all files..." && git add . && gitcomm && git push;;
     n|N ) echo "[ERROR] Exiting..."; exit 2;;
     * ) echo "[ERROR] Invalid, exiting..."; exit 3;;
   esac
 }
 
-# test if there are changed tracked files which are cached, 1 is cached changes and 0 is none.
-if git diff --cached --quiet ; then
+containerFunc() {
+  if ! command -v gitcomm >/dev/null; then
+    cd /tmp
+    go get -u -d github.com/angelofdeauth/gitcomm/cmd
+    go build -o "${GOPATH}"/bin/gitcomm ./cmd/...
+    cd -
+  fi
   
-  # test if there are changed tracked files which are not cached, 1 is uncached changes and 0 is none.
-  if git diff-index "$(git write-tree)" --quiet -- ; then
-    if [ -n "$(git status -s)" ]; then
-      echo "No changes detected either cached, new, or staged."
+  git status
+
+  # test if there are changed tracked files which are cached, 1 is cached changes and 0 is none.
+  if git diff --cached --quiet ; then
+    
+    # test if there are changed tracked files which are not cached, 1 is uncached changes and 0 is none.
+    if git diff-index "$(git write-tree)" --quiet -- ; then
+
+      # test if there are any uncommitted changes between the current filesystem and HEAD (catchall for new files).
+      if [ -n "$(git status -s)" ]; then
+        echo "No changes detected either cached, new, or staged."
+      else
+        conditionalAddAll
+      fi
     else
       conditionalAddAll
     fi
   else
-    conditionalAddAll
-  fi
-else
-  git commit
-  git push
-fi
+    git diff --cached
+    gitcomm
+    git push
+  fi 
+  echo "[OK] Commit completed!"
+}
 
-echo "[OK] Commit completed!"
+containerGen() {
+  podman run --rm \
+    --env IS_CONTAINER=TRUE \
+    -v "$PWD":/usr/src/"$(basename "$PWD")":z \
+    -w /usr/src/"$(basename "$PWD")" \
+    golang:1.14 \
+    ./hack/commit.sh
+}
+
+conditional() {
+  if command -v gitcomm >/dev/null; then
+    containerFunc
+  else
+    containerGen
+  fi
+}
+
+conditional
