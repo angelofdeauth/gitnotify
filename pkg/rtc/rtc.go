b7c6855b99e28533df2768704d3871923d67e6d7
Antonio Escalera <aj@angelofdeauth.host>
2020-04-03 18:47:02 -0500

    feat(rtc/service/daemonargs): RunTimeConfig.Service structure provides `DaemonArgs` field
    Add support for the RunTimeConfig.Service.DaemonArgs field.
    Add support for the user to pass daemon arguments to override the
    defaults in the `service` command.
    
    [issue #10]
    
    * Tags: rtc/service

--- pkg/rtc/rtc.go
+++ pkg/rtc/rtc.go
@@ -1,6 +1,6 @@
 // @File:     rtc.go
 // @Created:  2020-03-28 21:05:29
-// @Modified: 2020-03-29 23:54:13
+// @Modified: 2020-04-03 12:02:50
 // @Author:   Antonio Escalera
 // @Commiter: Antonio Escalera
 // @Mail:     aj@angelofdeauth.host
@@ -26,6 +26,7 @@ type RunTimeCfg struct {
 // Service is the object for runtime configuration passed to the Service command.
 type Service struct {
 	Start       bool
+	DaemonArgs  string
 	InstallPath string
 	OutputPath  string
 	User        string
